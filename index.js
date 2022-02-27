21ca3b9280bf3435fd891348ba4a1f2a3feece82
lanny lopez <lannylynnlopez89@gmail.com>
2022-02-27 08:01:29 -0700

    generated page

---
+++ index.js
@@ -0,0 +1,173 @@
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const genHTML = require('./src/generator');
+
+const employeeArray = [];
+const addManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Who is the manager of the team?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Name is required!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the manager's email.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid email!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Please enter the Manager's office number.",
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log('Please enter a valid office number!');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+    ])
+    .then(managerInput => {
+        const { name, id, email, officeNumber } = managerInput;
+        const manager = new Manager (name, id, email, officeNumber);
+        employeeArray.push(manager);
+        console.log(manager);
+    })
+};
+const addEmployee = () => {
+    console.log(' - - - Add Employees - - - ');
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'roles',
+            message: "Please select your Employee's role",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the Employee's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a name!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the Employee's email.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log("Please enter a valid email!")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Please enter the Employee's github username.",
+            when: (input) => input.roles === 'Engineer',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid github username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Please enter the Employee's school.",
+            when: (input) => input.roles === 'Intern',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a school!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmEmployee',
+            message: 'Would you like to add another employee?',
+            default: false
+        }
+    ])
+    .then (employeeData => {
+        let { name, id, email, roles, github, school, confirmEmployee } = employeeData;
+        let employee;
+        if (roles === 'Intern') {
+            employee = new Intern (name, id, email, github);
+            console.log(employee);
+        } else if (roles ==='Engineer') {
+            employee = new Engineer (name, id, email, school);
+            console.log(employee);
+        }
+        employeeArray.push(employee);
+        if (confirmEmployee) {
+            return addEmployee(employeeArray);
+        } else {
+             return employeeArray;
+         }
+     })
+ }; 
+ 
+
+ const writeFile = data => {
+     fs.writeFile('./dist/index.html', data, err => {
+         if (err) {
+             console.log(err);
+             return;
+         } else {
+             console.log("Your team page has been generated! Congrats!")
+         }
+     })
+ };
+
+ addManager()
+     .then(addEmployee)
+     .then(employee => {
+         return genHTML(employeeArray);
+     })
+     .then(pageHTML => {
+         return writeFile(pageHTML);
+     })
+     .catch(err => {
+         console.log(err);
+     });
--- src/generator.js
+++ src/generator.js
@@ -1,77 +1,91 @@
-const genIntern = function (intern){
+const genIntern = function (intern) {
     return `
     <div>
-        <h3>${intern.name}</h3>
+        <h4>${intern.name}</h4>
         <span>Intern</span>
         <ul>
-            <li>${intern.id}</li>
+            <li>ID: ${intern.id}</li>
+            <li>
+                Email: <a href="mailto:${intern.email}">${intern.email}</a>
+            </li>
             <li>${intern.school}</li>
-            <li>${intern.email}</li>
         </ul>
     </div>
-        `;
+    `;
 }
 
-const genEngineer = function(engineer){
-    `<div>
-    <h3>${engineer.name}</h3>
-    <span>Engineer</span>
-    <ul>
-        <li>${engineer.id}</li>
-        <li>${engineer.school}</li>
-        <li>${engineer.email}</li>
-    </ul>
-</div>`;
-}
-const genManager = function(manager){
-    `<div>
-    <h3>${manager.name}</h3>
-    <span>Manager</span>
-    <ul>
-        <li>${manager.id}</li>
-        <li>${manager.school}</li>
-        <li>${manager.email}</li>
-    </ul>
-</div>`;
+const genEngineer = function (engineer) {
+    return `
+    <div>
+        <h4>${engineer.name}</h4>
+        <span>Engineer</span>
+        <ul>
+            <li>ID: ${engineer.id}</li>
+            <li>
+                Email: <a href="mailto:${engineer.email}">${engineer.email}</a>
+            </li>
+            <li>
+                Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a>
+            </li>
+        </ul>
+    </div>
+    `;
 }
 
-genHTML = (data) =>{
-    employeeArray =[];
+const genManager = function (manager) {
+    return `
+    <div>
+        <h4>${manager.name}</h4>
+        <span>Manager</span>
+        <ul>
+            <li>ID: ${manager.id}</li>
+            <li>
+                Email: <a href="mailto:${manager.email}">${manager.email}</a>
+            </li>
+            <li>Office no. ${manager.office}</li>
+        </ul>
+    </div>
+    `;
+}
 
-    for (let i = 0; i < data.length; i++){
+genHTML = (data) => {
+    employeeArray = [];
+
+    for (let i = 0; i < data.length; i++) {
         const employee = data[i];
         const role = employee.getRole();
 
-        if(role === 'intern'){
+        if (role === 'Intern') {
             const internDiv = genIntern(employee);
 
-            employeeArray.push(internDiv)
+            employeeArray.push(internDiv);
         }
 
-        if(role === 'Engineer'){
+        if (role === 'Engineer') {
             const engineerDiv = genEngineer(employee);
 
-            employeeArray.push(engineerDiv)
+            employeeArray.push(engineerDiv);
         }
 
-        if(role === 'Manager'){
+        if (role === 'Manager') {
             const managerDiv = genManager(employee);
 
-            employeeArray.push(managerDiv)
+            employeeArray.push(managerDiv);
         }
     }
+
     const employeeDiv = employeeArray.join('');
     const genTeam = genPage(employeeDiv);
     return genTeam;
 }
 
-const genPage = function(employeeDiv){
+const genPage = function (employeeDiv) {
     return`
-    <!DOCTYPE HTML>
-    <html lang="em">
+    <!DOCTYPE html>
+    <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <title>TEAM GENERATOR</title>
+	    <meta charset="utf-8">
+	    <title>Team Generator</title>
     </head>
     <body>
         <main>
@@ -79,7 +93,7 @@ const genPage = function(employeeDiv){
         </main>
     </body>
     </html>
-    `
+    `;
 }
 
 module.exports = genHTML;
